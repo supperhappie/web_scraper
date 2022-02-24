3460cc0e3649eecc3c1ef009261343021f632208
supperhappie <kwtdevel@gmail.com>
2022-02-25 01:13:09 +0900

    dadtatype : list, tuple, dict

---
+++ datatype.py
@@ -0,0 +1,8 @@
+days_l =['mon', 'tue', 'wed', 'thur', 'fri', 'sa', 'sun'];
+days_t =('mon', 'tue', 'wed', 'thur', 'fri', 'sa', 'sun');
+days_d ={1:'mon', 2:'tue', 3:'wed', 4:'thur', 5:'fri', 6:'sa', 7:'sun'};
+print(days_d)
+for i in days_d.keys():
+    print(i)
+for i in days_d.values():
+    print(i)
